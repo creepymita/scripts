f6d29410190b82970ad792003d951d48a3b0c4d7
Mita <sasunimu@gmail.com>
2024-12-26 22:22:52 +0000

    First Commit

---
+++ autostart.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Monitor
+xrandr --output eDP1 --mode 1366x768 --pos 1920x312 --rotate normal --output DP1 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VGA1 --off --output VIRTUAL1 --off
+
+# NetowrkManager and Pulseaudio applet
+nm-applet &
+pa-applet &
+
+# Restore wallpaper
+nitrogen --restore &
+
+# Admim Authentication
+/usr/libexec/polkit-mate-authentication-agent-1 &
+
+# Animations
+picom --config ~/.config/picom/picom.conf &
+
+# Status bar for dwm
+slstatus &
+
+# Launch Software
+flameshot &
+
+# Ajust brightness on the second monitor
+xbacklight -set 15 &
+
+# Disable monitor sleep on xorg
+xset r rate 300 500 &
+xset s off &
+xset -dpms &
---
+++ energy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+style="power"
+rofi_command="rofi -theme /usr/share/rofi/themes/dmenu.rasi"
+
+# Options
+shutdown="Encerrar"
+reboot="Reinciar"
+
+# Variable passed to rofi
+options="$shutdown\n$reboot"
+
+chosen="$(echo -e "$options" | $rofi_command -p " Energia " -dmenu -selected-row 2)"
+
+case $chosen in
+    $shutdown)
+    	doas /usr/bin/shutdown -h now
+	;;
+    $reboot)
+    	doas /usr/bin/shutdown -r now
+        ;;
+esac
